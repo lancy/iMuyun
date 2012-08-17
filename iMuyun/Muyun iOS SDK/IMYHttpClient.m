4aa6d81cd47ee7e8ae36eb2130f4669f6dd34f74
lancy <lancy1014@gmail.com>
2012-08-17 17:43:23 +0800

    try to move directorys

--- iMuyun/Muyun iOS SDK/IMYHttpClient.m
+++ iMuyun/Muyun iOS SDK/IMYHttpClient.m
@@ -16,7 +16,6 @@
 // http://omegaga.net:8000/
 
 //static NSString* const kHost = @"http://222.200.181.42/";
-//static NSString* const kHost = @"http://omegaga.net/imuyun/";
 static NSString* const kHost = @"http://omegaga.net:8000/";
 static NSString* const kLogin = @"login/";
 static NSString* const kRegister = @"register/";
