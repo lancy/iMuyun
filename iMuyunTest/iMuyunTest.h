afe43100390a6cbcdb976c882073aaa2986a4312
lancy <lancy1014@gmail.com>
2012-08-23 17:34:57 +0800

    unit test

---
+++ iMuyunTest/iMuyunTest.h
@@ -0,0 +1,13 @@
+//
+//  iMuyunTest.h
+//  iMuyunTest
+//
+//  Created by Lancy on 23/8/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iMuyunTest : SenTestCase
+
+@end
---
+++ iMuyunTest/iMuyunTest.m
@@ -0,0 +1,32 @@
+//
+//  iMuyunTest.m
+//  iMuyunTest
+//
+//  Created by Lancy on 23/8/12.
+//
+//
+
+#import "iMuyunTest.h"
+
+@implementation iMuyunTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iMuyunTest");
+}
+
+@end
