10346e624b7227c04494a12b54a571b6ded4f9b2
lancy <lancy1014@gmail.com>
2012-09-13 13:52:38 +0800

    Revert "fixed recents need to reload"
    This reverts commit 60da8761badd630219e14a4e2cd1cdff8b58d022.

--- iMuyun/UI/Call View Controller/IMYVideoCallViewController.m
+++ iMuyun/UI/Call View Controller/IMYVideoCallViewController.m
@@ -273,11 +273,6 @@ static NSString* const kUserName = @"lancy";
 - (IBAction)tapEndButton:(id)sender {
 //    [[IMYHttpClient shareClient] requestEndVideoCallWithUsername:self.username delegate:self];
     NSLog(@"User tap end button");
-    
-    // recents need to reload
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setValue:@"No" forKey:@"rencentsNeedToReload"];
-
     [self.session disconnect];
     if ([self presentingViewController] != nil) {
         [self dismissModalViewControllerAnimated:YES];
